6f73b48b184f6e2dbb139ffacdcfaf6b3ceff7e7
AlonW <alws34@gmail.com>
2023-01-13 17:35:04 +0200

    init commit
    -input validation
    -edge case validations
    -sha 512 calculation (of any file)
    -saving the sha512 signature in a file for future reference (the file is being saved at the .exe location)

---
+++ Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace ShaCalculator
+{
+    partial class frmShaCalculator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbConsole = new System.Windows.Forms.ListBox();
+            this.tbPath = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbHash2 = new System.Windows.Forms.TextBox();
+            this.tbHash1 = new System.Windows.Forms.TextBox();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.pnlRes = new System.Windows.Forms.Panel();
+            this.lblRes = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.pnlRes.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lbConsole
+            // 
+            this.lbConsole.Dock = System.Windows.Forms.DockStyle.Right;
+            this.lbConsole.FormattingEnabled = true;
+            this.lbConsole.HorizontalScrollbar = true;
+            this.lbConsole.Location = new System.Drawing.Point(415, 0);
+            this.lbConsole.Name = "lbConsole";
+            this.lbConsole.Size = new System.Drawing.Size(385, 412);
+            this.lbConsole.TabIndex = 0;
+            // 
+            // tbPath
+            // 
+            this.tbPath.Location = new System.Drawing.Point(5, 22);
+            this.tbPath.Name = "tbPath";
+            this.tbPath.Size = new System.Drawing.Size(394, 20);
+            this.tbPath.TabIndex = 1;
+            this.tbPath.TextChanged += new System.EventHandler(this.tbPath_TextChanged);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.tbHash2);
+            this.panel1.Controls.Add(this.tbHash1);
+            this.panel1.Controls.Add(this.tbPath);
+            this.panel1.Location = new System.Drawing.Point(4, 4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(405, 144);
+            this.panel1.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(8, 99);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(83, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Compared Hash";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(8, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Calculated Hash";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 5);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "FilePath";
+            // 
+            // tbHash2
+            // 
+            this.tbHash2.Location = new System.Drawing.Point(5, 115);
+            this.tbHash2.Name = "tbHash2";
+            this.tbHash2.Size = new System.Drawing.Size(394, 20);
+            this.tbHash2.TabIndex = 3;
+            this.tbHash2.TextChanged += new System.EventHandler(this.tbHash1_TextChanged);
+            // 
+            // tbHash1
+            // 
+            this.tbHash1.Location = new System.Drawing.Point(5, 67);
+            this.tbHash1.Name = "tbHash1";
+            this.tbHash1.Size = new System.Drawing.Size(394, 20);
+            this.tbHash1.TabIndex = 2;
+            this.tbHash1.TextChanged += new System.EventHandler(this.tbHash1_TextChanged);
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(4, 356);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(86, 52);
+            this.btnBrowse.TabIndex = 3;
+            this.btnBrowse.Text = "browse...";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // pnlRes
+            // 
+            this.pnlRes.Controls.Add(this.lblRes);
+            this.pnlRes.Location = new System.Drawing.Point(116, 154);
+            this.pnlRes.Name = "pnlRes";
+            this.pnlRes.Size = new System.Drawing.Size(221, 99);
+            this.pnlRes.TabIndex = 4;
+            this.pnlRes.Visible = false;
+            // 
+            // lblRes
+            // 
+            this.lblRes.AutoSize = true;
+            this.lblRes.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRes.Location = new System.Drawing.Point(89, 39);
+            this.lblRes.Name = "lblRes";
+            this.lblRes.Size = new System.Drawing.Size(29, 16);
+            this.lblRes.TabIndex = 7;
+            this.lblRes.Text = "res";
+            // 
+            // frmShaCalculator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.ClientSize = new System.Drawing.Size(800, 412);
+            this.Controls.Add(this.pnlRes);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.lbConsole);
+            this.Name = "frmShaCalculator";
+            this.Text = "Sha Calculator";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.pnlRes.ResumeLayout(false);
+            this.pnlRes.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lbConsole;
+        private System.Windows.Forms.TextBox tbPath;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbHash2;
+        private System.Windows.Forms.TextBox tbHash1;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.Panel pnlRes;
+        private System.Windows.Forms.Label lblRes;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace ShaCalculator
+{
+    public partial class frmShaCalculator : Form
+    {
+        public frmShaCalculator()
+        {
+            InitializeComponent();
+            CenterToScreen();
+        }
+
+        private void tbPath_TextChanged(object sender, EventArgs e)
+        {
+            string text = tbPath.Text;
+            Regex re = new Regex(@"^(?:[a-zA-Z]\:|\\\\[\w\.]+\\[\w.$]+)\\(?:[\w ]+\\)*[\w ]+\.[\w]+$");
+           //Regex re2 = new Regex(@"^(?:[a-zA-Z]\:|\\\\[\w\.]+\\[\w.$]+)\\(?:[\w]+\\)*\w([\w.])+$");
+            if (string.IsNullOrEmpty(text) || !re.IsMatch(text))
+            {
+                lbConsole.Items.Add("Invalid file path provided");
+                return;
+            }
+
+            if (!File.Exists(text))
+            {
+                lbConsole.Items.Add("File Does not exist");
+                return;
+            }
+            tbHash1.Text = "";
+            tbHash2.Text = "";
+            pnlRes.Visible = false;
+
+            string hash = GetHash(text);
+            tbHash1.Text = hash;
+            string fileName = Path.GetFileName(text);
+            lbConsole.Items.Add($"Calculated hash for file {fileName}\nCapathed hash (SHA512):\t\t{hash}");
+            string saveFile = $"{fileName}_SHA512.txt";
+            if (!File.Exists(saveFile))
+                File.Create(saveFile).Close();
+
+            using (StreamWriter sw = new StreamWriter($"{fileName}_SHA512.txt"))
+                sw.WriteLine($"{DateTime.Now.ToString("dd/MM/yyyy HH:mm tt")}\n{text}\nSHA512 signature:\n{hash}");
+
+        }
+
+
+        public static string GetHash(string filePath)
+        {
+            using (SHA512 sha512 = SHA512.Create())
+            {
+                using (FileStream stream = File.OpenRead(filePath))
+                {
+                    byte[] hash = sha512.ComputeHash(stream);
+                    return BitConverter.ToString(hash).Replace("-", "").ToLowerInvariant();
+                }
+            }
+        }
+
+        private void tbHash1_TextChanged(object sender, EventArgs e)
+        {
+            TextBox tb = sender as TextBox;
+            if (tb == null)
+                return;
+            string text = tb.Text;
+            string msg;
+
+            Regex hashRegex = new Regex(@"^[a-f0-9]{128}$");
+
+            if (string.IsNullOrWhiteSpace(text))
+            {
+                lbConsole.Items.Add("Invalid input provided");
+                return;
+            }
+            if (!hashRegex.IsMatch(text))
+            {
+                lbConsole.Items.Add("Input is not a valid sha512 hash");
+                if (pnlRes.Visible) {
+                    pnlRes.BackColor = Color.Red;
+                    lblRes.ForeColor = Color.White;
+                    msg = "Hashes DONT match!";
+                    lblRes.Text = msg;
+                    CenterLabelInPanel(lblRes, pnlRes);
+
+                    if (!pnlRes.Visible)
+                        pnlRes.Visible = true;
+                }
+                return;
+            }
+
+            if (!string.IsNullOrWhiteSpace(tbHash1.Text) && !string.IsNullOrWhiteSpace(tbHash2.Text))
+            {
+                if (tbHash1.Text == tbHash2.Text)
+                {
+                    pnlRes.BackColor = Color.Green;
+                    lblRes.ForeColor = Color.Black;
+                    msg = "Hashes match!";
+                }
+                else
+                {
+                    pnlRes.BackColor = Color.Red;
+                    lblRes.ForeColor = Color.White;
+                    msg = "Hashes DONT match!";
+                }
+                if (!pnlRes.Visible)
+                    pnlRes.Visible = true;
+
+                lblRes.Text = msg;
+                lbConsole.Items.Add(msg);
+                CenterLabelInPanel(lblRes, pnlRes);
+            }
+
+        }
+
+        public static string GetFilePath()
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.InitialDirectory = "c:\\";
+            openFileDialog.Filter = "All files (*.*)|*.*";
+            openFileDialog.FilterIndex = 1;
+            openFileDialog.RestoreDirectory = true;
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                return openFileDialog.FileName;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public void CenterLabelInPanel(Label label, Panel panel)
+        {
+            label.AutoSize = true;
+            label.Location = new Point((panel.Width - label.Width) / 2, (panel.Height - label.Height) / 2);
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            tbPath.Text = GetFilePath();
+        }
+    }
+}
